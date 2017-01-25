69c68eb7f4d18c82e6ffde79112c6e912437c39f
Julien <julien.fournier@ynov.com>
2017-01-25 11:06:24 +0100

    fix meteo

--- application/views/meteo.php
+++ application/views/meteo.php
@@ -25,7 +25,7 @@ foreach ($infos as $info){
           <h3>Infos :</h3>
           <div class="row">
             <?php foreach ($meteos as $key_meteo=>$meteo) : ?>
-              <div id="meteo-<?php echo $key_meteo; ?>" class="col-md-12 row-eq-height animated <?php if($key_meteo == 0) echo 'flipInX'; else  echo 'hidden'; ?>" style="color: white;<?php if($key_meteo!=1)echo 'background-color: #ec4363;'; else echo 'background-color: #00AE9C;'; ?> min-height: 180px;">
+              <div id="meteo-<?php echo $key_meteo; ?>" class="col-md-12 row-eq-height animated <?php if($key_meteo == 0) echo 'flipInX'; else  echo 'hidden'; ?>" style="color: white;<?php if($key_meteo!=1)echo 'background-color: #ec4363;'; else echo 'background-color: rgb(0, 174, 156);'; ?> min-height: 180px;">
                 <div class="col-md-7">
                   <h4 style="margin-left:5px;"><?php echo $location; ?> - Météo <?php if($key_meteo == 0) echo 'du jour'; else echo 'de Demain'; ?></h4>
                   <ul>Températures :
@@ -57,7 +57,7 @@ foreach ($infos as $info){
           </div>
           <div class="row" style="min-height: 10px;"></div>
           <div class="row">
-            <div class="col-md-7" style="color: white;background-color: #00AE9C;">
+            <div class="col-md-7" style="color: white;background-color: rgb(51, 225, 207);">
               <h4 style="margin-left:5px;">Anniversaires du jour :</h4>
               <?php if(count($anniversaire_etu) == 0 && count($anniversaire_inter) == 0): ?>
                 <center>
