407b431b1c0b524a2cad5429c40ca66a117403ca
Julien <julien.fournier@ynov.com>
2017-02-02 16:06:46 +0100

    head

--- application/views/templates/_header.php
+++ application/views/templates/_header.php
@@ -11,7 +11,7 @@ $moduleD = "_".$current_config['moduleDroite'].".php";
 
 if($moduleG == "_logo.php"){
 	$moduleGinnerHTML = 'class="col-md-2"';
-	$moduleGStyle = "height:150px;";
+	$moduleGStyle = "height: 20vh;";
 }
 else if($moduleG == "_date.php"){
 	$moduleGinnerHTML = 'class="col-md-5 date"';
