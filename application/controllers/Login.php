8ff8dd62566759eb57f27fde83c1d6bde0e6df4b
Clement <clem_ios@hotmail.com>
2017-03-21 16:11:23 +0100

    Redirection vers /admin en cas de session et de request sur la base_url

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,13 +11,17 @@ class Login extends CI_Controller {
 
  function index()
  {
-
-	$session_data = $this->session->userdata('logged_in');
-	$data['username'] = $session_data['username'];
-	$data['current_config'] = $this->data_model->get_config_tv("news");
-	$this->template->set('title', 'Login');
-	$this->template->load('templates/admin', 'login', $data);
- }
+	if($this->session->userdata('logged_in')){
+		$session_data = $this->session->userdata('logged_in');
+		redirect('admin', 'refresh');
+	} else {
+		$session_data = $this->session->userdata('logged_in');
+		$data['username'] = $session_data['username'];
+		$data['current_config'] = $this->data_model->get_config_tv("news");
+		$this->template->set('title', 'Login');
+		$this->template->load('templates/admin', 'login', $data);
+ 	}
+}
 
 }
 
