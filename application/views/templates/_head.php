57c329dffec3bf48f5f64e9985bfa1b7b086634c
Clement <clem_ios@hotmail.com>
2017-03-21 15:54:27 +0100

    Réécriture template admin et component _head

--- application/views/templates/_head.php
+++ application/views/templates/_head.php
@@ -4,14 +4,21 @@
   </title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link href="<?= constant('ASSETS') ?>/animate.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link href="<?= constant('VENDORS') ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= constant('ASSETS') ?>/style.css" rel="stylesheet">
+  
   <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?= constant('ASSETS') ?>/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?= constant('ASSETS') ?>/icon/iconvault-preview.css" />
+  <link href="<?= constant('VENDORS') ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= constant('ASSETS') ?>/animate.css" rel="stylesheet">
+  <link href="<?= constant('ASSETS') ?>/style.css" rel="stylesheet">
+
+  
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="<?= constant('VENDORS') ?>/bootstrap/js/bootstrap.min.js"></script> 
   <script src="<?= constant('ASSETS'); ?>/tinymce/js/tinymce/tinymce.min.js"></script>
+  <script src="<?= constant('VENDORS') ?>/bootstrap-notify/bootstrap-notify.js"></script>
+  
   <script>
     tinymce.init({
       selector: '#text',
@@ -26,11 +33,4 @@
       content_css: '//www.tinymce.com/css/codepen.min.css'
     });
   </script>
-  <script src="http://code.jquery.com/jquery-latest.js">
-  </script>
-  <script src="<?= constant('VENDORS') ?>/bootstrap-notify/bootstrap-notify.js"></script>
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
--- application/views/templates/admin.php
+++ application/views/templates/admin.php
@@ -1,83 +1,79 @@
 <html lang="fr">
 <?php  include('_head.php'); ?>
-	<body>
-<?php  include('_header.php'); ?>
+<body>
+	<?php  include('_header.php'); ?>
 
-<?= $contents ?>
+	<?= $contents ?>
 
-<footer>
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
-    <div class="feedback right">
-      <div class="tooltips">
-          <div class="btn-group dropup">
-            <button type="button" class="btn btn-danger dropdown-toggle btn-circle btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-bug fa-2x" title="Report Bug"></i>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-right dropdown-menu-form">
-              <li>
-                <div class="report">
-                  <h2 class="text-center">Signaler un bug</h2>
-                  <form id="contactForm" class="doo" method="post" action="<?php echo  site_url('data/report_bug'); ?>">
-                    <div class="col-sm-12">
-                      <textarea required name="comment" class="form-control" placeholder="Merci de nous signaler les éventuels beugs ou correctifs à apporté pour nous aider a améliorer votre experience"></textarea>
-                      <input name="screenshot" type="hidden" class="screen-uri">
-                      <span class="screenshot pull-right"><i class="fa fa-camera cam" title="Take Screenshot"></i></span>
-                     </div>
-										 <div class="col-sm-12" id="screen">
-										 Appuyez sur <i class="fa fa-camera cam"></i> pour joindre une capture automatique ;)
-                     </div>
-										 <div class="col-sm-12 clearfix">
-                      <button class="btn btn-primary btn-block">Envoyer le rapport</button>
-                     </div>
-                 </form>
-                </div>
-                <div class="loading text-center hideme">
-                  <h2>Veuillez patienter...</h2>
-                  <h2><i class="fa fa-refresh fa-spin"></i></h2>
-                </div>
-                <div class="reported text-center hideme">
-                  <h2>Merci !</h2>
-                  <p>Nous avons bien reçu votre signalement, merci pour votre aide.</p>
-                   <div class="col-sm-12 clearfix">
-                      <button class="btn btn-success btn-block do-close">Fermer</button>
-                   </div>
-                </div>
-                <div class="failed text-center hideme">
-                  <h2>Oh no!</h2>
-                  <p>Il semblerai que nous n'ayons pas reçu votre rapport.<br><br><a href="mailto:julien.fournier@ynov.com">Essayez de nous envoyer un mail.</a></p>
-                   <div class="col-sm-12 clearfix">
-                      <button class="btn btn-danger btn-block do-close">Fermer</button>
-                   </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-      </div>
-    </div>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+	<footer>
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+		<div class="feedback right">
+			<div class="tooltips">
+				<div class="btn-group dropup">
+					<button type="button" class="btn btn-danger dropdown-toggle btn-circle btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fa fa-bug fa-2x" title="Report Bug"></i>
+					</button>
+					<ul class="dropdown-menu dropdown-menu-right dropdown-menu-form">
+						<li>
+							<div class="report">
+								<h2 class="text-center">Signaler un bug</h2>
+								<form id="contactForm" class="doo" method="post" action="<?php echo  site_url('data/report_bug'); ?>">
+									<div class="col-sm-12">
+										<textarea required name="comment" class="form-control" placeholder="Merci de nous signaler les éventuels beugs ou correctifs à apporté pour nous aider a améliorer votre experience"></textarea>
+										<input name="screenshot" type="hidden" class="screen-uri">
+										<span class="screenshot pull-right"><i class="fa fa-camera cam" title="Take Screenshot"></i></span>
+									</div>
+									<div class="col-sm-12" id="screen">
+										Appuyez sur <i class="fa fa-camera cam"></i> pour joindre une capture automatique ;)
+									</div>
+									<div class="col-sm-12 clearfix">
+										<button class="btn btn-primary btn-block">Envoyer le rapport</button>
+									</div>
+								</form>
+							</div>
+							<div class="loading text-center hideme">
+								<h2>Veuillez patienter...</h2>
+								<h2><i class="fa fa-refresh fa-spin"></i></h2>
+							</div>
+							<div class="reported text-center hideme">
+								<h2>Merci !</h2>
+								<p>Nous avons bien reçu votre signalement, merci pour votre aide.</p>
+								<div class="col-sm-12 clearfix">
+									<button class="btn btn-success btn-block do-close">Fermer</button>
+								</div>
+							</div>
+							<div class="failed text-center hideme">
+								<h2>Oh no!</h2>
+								<p>Il semblerai que nous n'ayons pas reçu votre rapport.<br><br><a href="mailto:julien.fournier@ynov.com">Essayez de nous envoyer un mail.</a></p>
+								<div class="col-sm-12 clearfix">
+									<button class="btn btn-danger btn-block do-close">Fermer</button>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 
-	<div class="col-md-6 col-md-offset-3" style="text-align: center;">
-		<?php if($this->session->userdata('logged_in')): ?> 
-			<a href="<?php echo site_url('logout'); ?>" class="lien footer"></br>
-			<span class="majuscule"><?php  echo $username; ?></span>
-			<i class="fa fa-sign-out" aria-hidden="true"></i> (Déconnexion)</a></br>
-			<a href="<?php echo site_url('admin'); ?>" class="lien footer"> Administration</a></br>
-		<?php else: ?>
-			<a href="<?php echo site_url('login'); ?>" class="lien footer"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a></br>
-		<?php endif; ?>
-			<span class="footer">Page générée en <strong>{elapsed_time}</strong> secondes</span>
-			<br/>
+		<div class="col-md-6 col-md-offset-3" style="text-align: center;">
+			<?php if($this->session->userdata('logged_in')): ?> 
+				<a href="<?php echo site_url('logout'); ?>" class="lien footer"></br>
+				<span class="majuscule"><?php  echo $username; ?></span>
+				<i class="fa fa-sign-out" aria-hidden="true"></i> (Déconnexion)</a></br>
+				<a href="<?php echo site_url('admin'); ?>" class="lien footer"> Administration</a></br>
+			<?php else: ?>
+				<a href="<?php echo site_url('login'); ?>" class="lien footer"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a></br>
+			<?php endif; ?>
+			<span class="footer">Page générée en <strong>{elapsed_time}</strong> secondes</span><br/>
 			<span class="footer">Propulsé par <strong>YNOV intranet</strong> v2.1</span>
-	</div>
+		</div>
 </footer>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?= constant('VENDORS') ?>/bootstrap/js/bootstrap.min.js"></script>
+
 <script>
 (function ( $ ) {
-    $.fn.feedback = function(success, fail) {
-    	self=$(this);
+	$.fn.feedback = function(success, fail) {
+		self=$(this);
 		self.find('.dropdown-menu-form').on('click', function(e){e.stopPropagation()})
 
 		self.find('.screenshot').on('click', function(){
@@ -95,8 +91,8 @@
 			self.find('.reported, .failed').hide();
 			self.find('.report').show();
 			self.find('.cam').removeClass('fa-check').addClass('fa-camera');
-		    self.find('.screen-uri').val('');
-		    self.find('textarea').val('');
+			self.find('.screen-uri').val('');
+			self.find('textarea').val('');
 		});
 
 		failed = function(){
@@ -125,10 +121,7 @@
 $(document).ready(function () {
 	$('.feedback').feedback();
 });
-
-
 </script>
 
 </body>
-
 </html>
