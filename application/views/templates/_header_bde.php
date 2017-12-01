1a6979c0a10636e858d2dd3205078225ccfaa7f2
julienf31 <julien.fournier@ynov.com>
2017-12-01 17:54:58 +0100

    fix

--- application/views/templates/_header_bde.php
+++ application/views/templates/_header_bde.php
@@ -1,10 +1,3 @@
-<?php
-$city="Toulouse";
-$country="FR"; //Two digit country code
-$url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&APPID=c568aac6ef8e8c92ab7b8ad8e75cfe9e&units=metric&lang=fr";
-$json=file_get_contents($url);
-$weather=json_decode($json,true);
-?>
 <div class="container-fluid" id="head">
 	<div class="row" style="height: 20vh;">
 	<?php  include('_logo.php'); ?>
