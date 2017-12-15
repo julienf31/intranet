faf2c145ad2361145f2c8d1c902274cbf359a353
julienf31 <julien.fournier@ynov.com>
2017-12-15 14:01:51 +0100

    fix

--- application/views/templates/_infoNoTrust.php
+++ application/views/templates/_infoNoTrust.php
@@ -1,7 +1,7 @@
 <br><div class="col-md-10 col-md-offset-1">
     <center>
         <div class="alert alert-danger">
-            <i class="fa fa-warning"></i> Attention : Ce n'est un le site officiel d'ynov, uniquement l'affichage des écrans du couloir
+            <i class="fa fa-warning"></i> Attention : Ce n'est pas le site officiel d'ynov, uniquement l'affichage des écrans du couloir
         </div>
     </center>
 </div>
