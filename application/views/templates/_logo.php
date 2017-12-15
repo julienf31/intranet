a040cca76321c263f0780c1d22a17cde35539cfc
julienf31 <julien.fournier@ynov.com>
2017-12-15 15:58:28 +0100

    img

--- application/views/templates/_logo.php
+++ application/views/templates/_logo.php
@@ -1,1 +1,1 @@
-<a href="<?php echo site_url('admin'); ?>"><img src="<?php if(isset($config)){echo base_url().'uploads/'.$config['logo'];}; ?>" height="100%"></a>
+<a href="<?php echo site_url('admin'); ?>"><img class="img-resposive" src="<?php if(isset($config)){echo base_url().'uploads/'.$config['logo'];}; ?>" height="100%"></a>
